aa452068feb9c9bf854bf71a01f35f845f720ff7
Shestakovandrij <shestakovandrij@gmai.com>
2021-01-24 16:44:25 +0100

    adada

--- gulpfile.js
+++ gulpfile.js
@@ -65,6 +65,7 @@ function scripts() {
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/rateYo/src/jquery.rateyo.js',
+    'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
